1ee5e3ca6eb97d1c384ed476ca2ce24777be1b79
Matt Freestone <matt@ntechr.com>
2025-04-27 17:05:07 -0600

    Initial Commit

---
+++ TaleWeaver/TaleWeaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaleWeaverApp.swift
+//  TaleWeaver
+//
+//  Created by Matt Freestone on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TaleWeaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
